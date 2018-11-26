1ebfff86368e519d6c0651292e0411cd1897c04e
Kevin Wang <rocwang0316@gmail.com>
2018-11-25 18:03:39 -0800

    Refactored the get-index function

--- functions/get-index.js
+++ functions/get-index.js
@@ -4,19 +4,23 @@ const fs = require('fs');
 
 var html; // Save the html content to a global variable to reuse.
 
-const loadHtml = () => html ? html : new Promise((resolve, reject) => {
-  fs.readFile('static/index.html', 'utf-8', (err, data) => {
-    if (err) reject(err);
-    resolve(data);
+// Use fs to read static html
+const loadHtml = () => {
+  if (html) return html;
+  return new Promise((resolve, reject) => {
+    fs.readFile('static/index.html', 'utf-8', (err, data) => {
+      if (err) reject(err);
+      resolve(data);
+    });
   });
-});
+};
 
 // User a Lambda function to serve static content
 module.exports.handler = async (event, context, callback) => {
-  const html = await loadHtml();
+  const returnHtml = await loadHtml();
   const response = {
     statusCode: 200,
-    body: html,
+    body: returnHtml,
     headers: { // The default header is JSON
       'Content-Type': 'text/html; charset=UTF-8',
     },
