517617b6ef30e2df2e0feec44640b50aaa41cb89
Kevin Wang <rocwang0316@gmail.com>
2018-11-26 23:05:25 -0800

    Config to request a iam role to call the get-restaurants api

--- functions/get-index.js
+++ functions/get-index.js
@@ -1,8 +1,10 @@
 'use strict';
 
 const fs = require('fs');
-const Mustache = require('mustache');
+const Mustache = require('mustache'); // Template library.
 const axios = require('axios');
+const aws4 = require('aws4'); // Signing http request library.
+const URL = require('url'); // Come from node.js module
 
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thusday', 'Friday', 'Saturday'];
 
@@ -20,7 +22,23 @@ const loadHtml = () => {
 };
 
 // Load the restaurants data
-const getRestaurants = () => axios.get(process.env.restaurants_api);
+const getRestaurants = () => {
+  // Use AWS4 to sign the request
+  const url = URL.parse(process.env.restaurants_api);
+  const opts = {
+    host: url.hostname,
+    path: url.pathname,
+  };
+  aws4.sign(opts);
+  return axios.get(process.env.restaurants_api, {
+    headers: {
+      Host: opts.headers.Host,
+      'X-Amz-Date': opts.headers['X-Amz-Date'],
+      Authorization: opts.headers.Authorization,
+      'X-Amz-Security-Token': opts.headers['X-Amz-Security-Token'],
+    },
+  });
+};
 
 // User a Lambda function to serve static content
 module.exports.handler = async (event, context, callback) => {
