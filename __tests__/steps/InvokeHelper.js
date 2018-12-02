4010003a8cb563d254bed99a7781b024ebedba8f
Kevin Wang <rocwang0316@gmail.com>
2018-12-01 22:10:10 -0800

    Added viaHttp function to help the acceptance test.

--- __tests__/steps/InvokeHelper.js
+++ __tests__/steps/InvokeHelper.js
@@ -1,5 +1,58 @@
-const APP_ROOT = '../../';
+import aws4 from 'aws4';
+import axios from 'axios';
+import URL from 'url'; // Come from node.js module
 
+
+const APP_ROOT = '../../';
+const testMode = process.env.TEST_MODE; // Pass this from npm script to decide which kind of test will be run.
+
+// The helper to sign the request and return headers
+const signHttpRequest = (isIAM, realURL) => {
+  if (!isIAM) return {};
+  const url = URL.parse(realURL);
+  const opts = {
+    host: url.hostname,
+    path: url.pathname,
+  };
+  aws4.sign(opts);
+
+  const headers = {
+    Host: opts.headers.Host,
+    'X-Amz-Date': opts.headers['X-Amz-Date'],
+    Authorization: opts.headers.Authorization,
+    'X-Amz-Security-Token': opts.headers['X-Amz-Security-Token'],
+  };
+  // If 'X-Amz-Security-Token' does not exsit, delete it for the local test.
+  if (!headers['X-Amz-Security-Token']) delete headers['X-Amz-Security-Token'];
+  return headers;
+};
+
+// Invode the handler via a real api gateway to do the acceptance test
+const viaHttp = async (path, method = 'get', opts = { iam: false }) => new Promise(async (resolve, reject) => {
+  const url = `${process.env.TEST_ROOT}/${path}`;
+  console.log(`Invoking via HTTP ${url}`);
+  const headers = signHttpRequest(opts.iam, url);
+  if (opts.authHeader) headers.Authorization = opts.authHeader; // Set a Authorization header if function need a cognito user token
+  const options = {
+    method,
+    url,
+    data: opts.body,
+    headers,
+  };
+  if (!opts.body) delete options.data;
+  try {
+    const result = await axios(options);
+    resolve({
+      headers: { ...result.headers, 'Content-Type': result.headers['content-type'] },
+      body: result.data,
+      statusCode: result.status,
+    });
+  } catch (err) {
+    reject(err);
+  }
+});
+
+// Invoke the handler locally to do the integration test
 const viaHandler = (handlerName, event = {}, context = {}) => {
   const handler = require(`${APP_ROOT}/functions/${handlerName}`).handler;
 
@@ -16,11 +69,15 @@ const viaHandler = (handlerName, event = {}, context = {}) => {
   });
 };
 
-const invokeGetIndex = () => viaHandler('get-index');
+const invokeGetIndex = () => testMode === 'integration'
+  ? viaHandler('get-index') : viaHttp('');
 
-const invokeGetRestaurants = () => viaHandler('get-restaurants');
+const invokeGetRestaurants = () => testMode === 'integration'
+  ? viaHandler('get-restaurants') : viaHttp('restaurants', 'get', { iam: true });
 
-const invokeSearchRestaurants = theme => viaHandler('search-restaurants', { body: JSON.stringify({ theme }) });
+const invokeSearchRestaurants = (theme, authHeader) => testMode === 'integration'
+  ? viaHandler('search-restaurants', { body: JSON.stringify({ theme }), authHeader })
+  : viaHttp('restaurants/search', 'post', { iam: false, body: { theme }, authHeader });
 
 module.exports = {
   invokeGetIndex,
