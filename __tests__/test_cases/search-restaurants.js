b48a30768c05deee05da5154138ca0d09736bd82
Kevin Wang <rocwang0316@gmail.com>
2018-12-01 22:11:49 -0800

    Added beforeAll and afterAll to create and delete user from the Cognito user pool

--- __tests__/test_cases/search-restaurants.js
+++ __tests__/test_cases/search-restaurants.js
@@ -1,13 +1,21 @@
 import { invokeSearchRestaurants } from '../steps/InvokeHelper';
 import initEvns from '../steps/InitialEnvs';
+import { registerUser, deleteUser } from '../steps/CognitoUserHelper';
+
+let user;
 
 describe('get-index: invoke the Get / endpoint', () => {
   beforeAll(async () => {
     await initEvns();
+    user = await registerUser();
+  });
+
+  afterAll(async () => {
+    await deleteUser(user);
   });
 
   test('invoke search-restaurants function', async () => {
-    const res = await invokeSearchRestaurants('cartoon');
+    const res = await invokeSearchRestaurants('cartoon', user.idToken);
 
     expect(res.statusCode).toBe(200);
     expect(res.body.Items.length).toBe(4);
