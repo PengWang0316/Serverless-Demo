ac14c21b1e1b9dc57856cc2dc679b737f7c201a0
Kevin Wang <rocwang0316@gmail.com>
2018-12-11 21:27:35 -0800

    Separated the notify restaurant logic in order to reuse.

---
+++ libs/notify-restaurant.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+
+const kinesis = new AWS.Kinesis();
+const sns = new AWS.SNS();
+const streamName = process.env.order_events_stream;
+const restaurantTopicArn = process.env.restaurant_notification_topic;
+
+
+module.exports = async order => {
+  const pubReq = {
+    Message: JSON.stringify(order),
+    TopicArn: restaurantTopicArn,
+  };
+  await sns.publish(pubReq).promise(); // Publish to the sns
+
+  console.log(`Notified the restaurant [${order.restaurantName}] of order [${order.orderId}]`);
+
+  const data = { ...order }; // Clone order for Kinesis
+  data.eventType = 'restaurant_notified'; // Change the event type to a new value
+  const putRecordReq = {
+    Data: JSON.stringify(data),
+    PartitionKey: order.orderId,
+    StreamName: streamName,
+  };
+  await kinesis.putRecord(putRecordReq).promise();
+  console.log('Published \'restaurant_notified\' event to Kinesis.');
+};
