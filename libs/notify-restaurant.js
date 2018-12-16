13a99a9f1b9bbebffb1013a004f55d420cdd3724
Kevin Wang <rocwang0316@gmail.com>
2018-12-15 18:04:04 -0800

    Use the new cloudwatch library to put metrics.

--- libs/notify-restaurant.js
+++ libs/notify-restaurant.js
@@ -2,31 +2,42 @@
 
 const AWS = require('aws-sdk');
 
+const cloudwatch = require('../libs/cloudwatch'); // Use this library to put some metrics in
+
 const kinesis = new AWS.Kinesis();
 const sns = new AWS.SNS();
 const streamName = process.env.order_events_stream;
 const restaurantTopicArn = process.env.restaurant_notification_topic;
 
-const chance = require('chance').Chance();
+// const chance = require('chance').Chance();
 
 module.exports = async order => {
-  if (chance.bool({ likelihood: 75 })) throw new Error('boom'); // Create a 75% of failure for testing purpose
+  try {
+    // if (chance.bool({ likelihood: 75 })) throw new Error('boom'); // Create a 75% of failure for testing purpose
+    const pubReq = {
+      Message: JSON.stringify(order),
+      TopicArn: restaurantTopicArn,
+    };
+    // await sns.publish(pubReq).promise(); // Publish to the sns
+    // Put the sns executing time to the cloudwatch function and wait to flush
+    await cloudwatch.trackExecTime('SnsPublishLatency', () => sns.putRecord(pubReq).promise());
 
-  const pubReq = {
-    Message: JSON.stringify(order),
-    TopicArn: restaurantTopicArn,
-  };
-  await sns.publish(pubReq).promise(); // Publish to the sns
+    console.log(`Notified the restaurant [${order.restaurantName}] of order [${order.orderId}]`);
 
-  console.log(`Notified the restaurant [${order.restaurantName}] of order [${order.orderId}]`);
-
-  const data = { ...order }; // Clone order for Kinesis
-  data.eventType = 'restaurant_notified'; // Change the event type to a new value
-  const putRecordReq = {
-    Data: JSON.stringify(data),
-    PartitionKey: order.orderId,
-    StreamName: streamName,
-  };
-  await kinesis.putRecord(putRecordReq).promise();
-  console.log('Published \'restaurant_notified\' event to Kinesis.');
+    const data = { ...order }; // Clone order for Kinesis
+    data.eventType = 'restaurant_notified'; // Change the event type to a new value
+    const putRecordReq = {
+      Data: JSON.stringify(data),
+      PartitionKey: order.orderId,
+      StreamName: streamName,
+    };
+    // await kinesis.putRecord(putRecordReq).promise();
+    // Put the kinesis executing time to the cloudwatch function and wait to flush
+    await cloudwatch.trackExecTime('KinesisPutRecordLatency', () => kinesis.putRecord(putRecordReq).promise());
+    console.log('Published \'restaurant_notified\' event to Kinesis.');
+    cloudwatch.incrCount('NotifyRestaurantSuccess');
+  } catch (err) { // Put a metric to track the fialure time
+    cloudwatch.incrCount('NotifyRestaurantFailed');
+    throw err;
+  }
 };
--- libs/retry-notify-restaurant.js
+++ libs/retry-notify-restaurant.js
@@ -2,14 +2,23 @@
 
 const AWS = require('aws-sdk');
 
+const cloudwatch = require('../libs/cloudwatch');
+
 const sns = new AWS.SNS();
 
 module.exports = async order => {
-  const pubReq = {
-    Message: JSON.stringify(order),
-    TopicArn: process.env.retry_restaurant_notification_topic,
-  };
-  await sns.publish(pubReq).promise(); // Publish to the sns
+  try {
+    const pubReq = {
+      Message: JSON.stringify(order),
+      TopicArn: process.env.retry_restaurant_notification_topic,
+    };
+    // await sns.publish(pubReq).promise(); // Publish to the sns
+    // Put a metric to the cloud watch
+    await cloudwatch('SnsPublishLatency', () => sns.publish(pubReq).promise());
 
-  console.log(`Sent order [${order.orderId}] for [${order.restaurantName}] via sns to retry.`);
+    console.log(`Sent order [${order.orderId}] for [${order.restaurantName}] via sns to retry.`);
+  } catch (err) {
+    cloudwatch.incrCount('NotifyRestaurantQueued');
+    throw err;
+  }
 };
