5c59e7a1699015903a3bcca11b4c4abb5850c750
Kevin Wang <rocwang0316@gmail.com>
2018-12-19 16:17:49 -0800

    Fixed an issue about cloudwatch called wrong function.

--- libs/retry-notify-restaurant.js
+++ libs/retry-notify-restaurant.js
@@ -3,7 +3,7 @@
 const AWSXray = require('aws-xray-sdk');
 const AWS = AWSXray.captureAWS(require('aws-sdk')); // Use the X-Ray to capture all request makes through AWS sdk
 
-const cloudwatch = require('../libs/cloudwatch');
+const cloudwatch = require('./cloudwatch');
 
 const sns = new AWS.SNS();
 
@@ -15,7 +15,7 @@ module.exports = async order => {
     };
     // await sns.publish(pubReq).promise(); // Publish to the sns
     // Put a metric to the cloud watch
-    await cloudwatch('SnsPublishLatency', () => sns.publish(pubReq).promise());
+    await cloudwatch.trackExecTime('SnsPublishLatency', () => sns.publish(pubReq).promise());
 
     console.log(`Sent order [${order.orderId}] for [${order.restaurantName}] via sns to retry.`);
   } catch (err) {
