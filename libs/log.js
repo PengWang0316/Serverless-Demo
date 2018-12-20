3b020a205a483a6006bfc1adb118328e2b3d7660
Kevin Wang <rocwang0316@gmail.com>
2018-12-19 16:21:32 -0800

    Added support for collecting the correlation ids.

--- libs/log.js
+++ libs/log.js
@@ -1,10 +1,32 @@
 'use strict';
 
+const correlationIds = require('./correlation-ids');
+
 const LogLevels = {
   DEBUG: 0,
   INFO: 1,
   WARN: 2,
   ERROR: 3,
+};
+
+/*
+ * Some information we want to include in the log
+ * The full list of Lambda environment variables see here https://docs.aws.amazon.com/lambda/latest/dg/current-supported-versions.html
+*/
+const DEFAULT_CONTEXT = {
+  awsRegion: process.env.AWS_REGION || process.env.AWS_DEFAULT_REGION,
+  functionName: process.env.AWS_LAMBDA_FUNCTION_NAME,
+  functionVersion: process.env.AWS_LAMBDA_FUNCTION_VERSION,
+  functionMemorySize: process.env.AWS_LAMBDA_FUNCTION_MEMORY_SIZE,
+  state: process.env.ENVIRONMENT || process.env.STAGE,
+};
+
+/*
+ * Get the context from the gloab variable and put the new contexts we collect
+*/
+const getContext = () => {
+  const context = correlationIds.get();
+  return context ? { ...DEFAULT_CONTEXT, ...context } : context;
 };
 
 // default to debug if not specified
@@ -27,7 +49,8 @@ function isEnabled(level) {
  */
 function appendError(params, err) {
   return err ? Object.assign(
-    params || { },
+    {},
+    params || {},
     { errorName: err.name, errorMessage: err.message, stackTrace: err.stack },
   ) : params;
 }
@@ -44,7 +67,7 @@ function log(levelName, message, params) {
     return;
   }
 
-  const logMsg = Object.assign({ ...params });
+  const logMsg = { ...getContext(), ...params };
   logMsg.level = levelName;
   logMsg.message = message;
 
